214d26ff1974faae3be90d297c9876a9d9df0b11
Marco Hamann <marcohamann89@hotmail.de>
2019-05-31 17:56:38 +0200

    added graphql service

---
+++ OverwatchGraphQL/Schema/Query.cs
@@ -0,0 +1,23 @@
+﻿using OverwatchAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OverwatchGraphQL.Schema
+{
+    public class Query
+    {
+        private readonly OverwatchClient client;
+
+        public Query(OverwatchClient client)
+        {
+            this.client = client;
+        }
+
+        public Task<Player> GetPlayer(string name)
+        {
+            return client.GetPlayerAsync(name);
+        }
+    }
+}
